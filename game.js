dfe1053fc14f14f40b2606dafd96c5fad81ca696
MatthewH18 <90624248+Matthaynes@users.noreply.github.com>
2023-02-24 16:00:32 +0000

    tried to add try again button

--- game.js
+++ game.js
@@ -7,23 +7,23 @@ let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
-  if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
-    }
-    return
+  if (gameOver === true) {
+    document.getElementById('button').style.display = "block";
+    
   }
+  return
+}
 
 
-  window.requestAnimationFrame(main)
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+window.requestAnimationFrame(main)
+const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
 
-  lastRenderTime = currentTime
+lastRenderTime = currentTime
 
-  update()
-  draw()
+update()
+draw()
 }
 
 window.requestAnimationFrame(main)
